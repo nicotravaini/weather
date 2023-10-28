78f050b2ef5ec59de586a90081b7e443a60f30f5
Nico Travaini <nicotravaini@gmail.com>
2023-10-28 17:47:10 -0300

    mod zoom mapa

--- src/components/transporte/Mapa.jsx
+++ src/components/transporte/Mapa.jsx
@@ -11,7 +11,7 @@ export default function Mapa({ transportdata }) {
         iconAnchor: [15, 40]
     });
     return (
-        <MapContainer center={position} zoom={15} scrollWheelZoom={false}>
+        <MapContainer center={position} zoom={10} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
